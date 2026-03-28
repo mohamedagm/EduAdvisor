96d264bf44eda92c6d89d1a927d9c5429d0b8a03
mohamedagm <abogm44ud@gmail.com>
2026-03-28 15:46:45 +0200

    add colors&styles of Our Style Guide

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  // =======================
+  // Body – Inter
+  // =======================
+
+  /// body inter medium 18
+  static const TextStyle bodyInterMedium18 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 24 / 18,
+  );
+
+  /// body inter medium 14
+  static const TextStyle bodyInterMedium14 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 16 / 14,
+  );
+
+  /// body inter regular 12
+  static const TextStyle bodyInterRegular12 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 16 / 12,
+  );
+
+  /// inter reg 16
+  static const TextStyle interRegular16 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 24 / 16,
+  );
+
+  // =======================
+  // Headings
+  // =======================
+
+  /// Heading1 20 b
+  static const TextStyle heading1_20b = TextStyle(
+    fontFamily: 'PublicSans',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 24 / 20,
+  );
+
+  /// heading 2 poppins sb 18
+  static const TextStyle heading2PoppinsSb18 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 18 / 18,
+  );
+
+  /// heading 3 popins reg 16
+  static const TextStyle heading3PoppinsReg16 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 24 / 16,
+  );
+
+  // =======================
+  // Buttons
+  // =======================
+
+  /// bottuns text
+  static const TextStyle buttonsText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    height: 23 / 20,
+  );
+
+  // =======================
+  // Poppins
+  // =======================
+
+  /// poppins reg 14
+  static const TextStyle poppinsRegular14 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 20 / 14,
+  );
+}
