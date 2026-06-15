4c08f8793c3dd42ba5c4dda191ae3a552b9e7585
mohamedagm <abogm44ud@gmail.com>
2026-06-15 16:22:36 +0300

    Add secure storage service and keys for token management; update API interceptor for authorization

--- lib/core/api/api_interceptor.dart
+++ lib/core/api/api_interceptor.dart
@@ -1,15 +1,32 @@
 import 'package:dio/dio.dart';
 import 'package:edu_advisor/core/api/api_constants.dart';
+import 'package:edu_advisor/core/services/secure_storage_keys.dart';
+import 'package:edu_advisor/core/services/secure_storage_service.dart';
 
 class ApiInterceptor extends Interceptor {
-  ApiInterceptor({this.language = ApiConstants.defaultLanguage});
+  ApiInterceptor({
+    this.language = ApiConstants.defaultLanguage,
+    SecureStorageService? secureStorageService,
+  }) : _secureStorageService = secureStorageService ?? SecureStorageService();
 
   final String language;
+  final SecureStorageService _secureStorageService;
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
     options.headers['Accept-Language'] = language;
 
-    super.onRequest(options, handler);
+    final accessToken = await _secureStorageService.read(
+      key: SecureStorageKeys.accessToken,
+    );
+
+    if (accessToken != null && accessToken.isNotEmpty) {
+      options.headers['Authorization'] = 'Bearer $accessToken';
+    }
+
+    handler.next(options);
   }
 }
---
+++ lib/core/services/secure_storage_keys.dart
@@ -0,0 +1,10 @@
+class SecureStorageKeys {
+  const SecureStorageKeys._();
+
+  static const String accessToken = 'access_token';
+  static const String accessTokenExpiresAt = 'access_token_expires_at';
+  static const String refreshToken = 'refresh_token';
+  static const String refreshTokenExpiresAt = 'refresh_token_expires_at';
+  static const String userId = 'user_id';
+  static const String userRole = 'user_role';
+}
